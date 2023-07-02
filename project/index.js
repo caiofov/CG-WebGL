b69d704992b01a01b6e7b480176a138fd281b774
caiofov <cfoviana@gmail.com>
2023-07-02 18:16:40 -0300

    codigo do prof com alguns ajustes

--- project/index.js
+++ project/index.js
@@ -1,16 +1,24 @@
+var teximg = [];
+var texSrc = ["gato.jpg", "cachorro.png"].map(e => `img/${e}`);
+var loadTexs = 0;
+var gl;
+var prog;
+
+var angle = 0;
+
 /**
  * Retorna o elemento do webgl
  * @param {HTMLCanvasElement} canvas 
  * @returns {WebGLRenderingContext}
  */
 function getGL(canvas) {
-	var gl = canvas.getContext("webgl");
-	if (gl) return gl;
+    let gl = canvas.getContext("webgl");
+    if (gl) return gl;
 
-	gl = canvas.getContext("experimental-webgl");
-	if (gl) return gl;
+    gl = canvas.getContext("experimental-webgl");
+    if (gl) return gl;
 
-	throw "Contexto WebGL inexistente! Troque de navegador!"
+    throw "Contexto WebGL inexistente! Troque de navegador!"
 }
 
 /**
@@ -21,231 +29,165 @@ function getGL(canvas) {
  * @returns {WebGLShader}
  */
 function createShader(gl, shaderType, shaderSrc) {
-	var shader = gl.createShader(shaderType);
-	gl.shaderSource(shader, shaderSrc);
-	gl.compileShader(shader);
+    let shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, shaderSrc);
+    gl.compileShader(shader);
 
-	if (gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-		return shader;
-	throw "Erro de compilação: " + gl.getShaderInfoLog(shader);
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+        return shader;
+    throw "Erro de compilação: " + gl.getShaderInfoLog(shader);
 
-	gl.deleteShader(shader);
+    gl.deleteShader(shader);
 
 }
 
 /**
  * Cria o programa e linka os shaders
- * @param {WebGLRenderingContext} gl 
  * @param {WebGLShader} vtxShader 
  * @param {WebGLShader} fragShader 
  * @returns {WebGLProgram}
  */
 function createProgram(gl, vtxShader, fragShader) {
-	var prog = gl.createProgram();
-	if (!prog) throw "Erro de criação do programa"
+    let prog = gl.createProgram();
+    if (!prog) throw "Erro de criação do programa"
 
-	gl.attachShader(prog, vtxShader);
-	gl.attachShader(prog, fragShader);
-	gl.linkProgram(prog);
+    gl.attachShader(prog, vtxShader);
+    gl.attachShader(prog, fragShader);
+    gl.linkProgram(prog);
 
-	if (gl.getProgramParameter(prog, gl.LINK_STATUS)) return prog;
-	throw "Erro de linkagem: " + gl.getProgramInfoLog(prog)
+    if (gl.getProgramParameter(prog, gl.LINK_STATUS)) return prog;
+    throw "Erro de linkagem: " + gl.getProgramInfoLog(prog)
 
-	gl.deleteProgram(prog);
+    gl.deleteProgram(prog);
 
 
 }
 
-/**
- * Inicializa os shaders e cria o programa
- * @param {WebGLRenderingContext} gl 
- * @returns {WebGLProgram}
- */
-function getProgram(gl) {
-	//Inicializa shaders
 
-	/** @type {string} */
-	var vtxShSrc = document.getElementById("vertex-shader").text;
 
-	/** @type {string} */
-	var fragShSrc = document.getElementById("frag-shader").text;
-
-	var vtxShader = createShader(gl, gl.VERTEX_SHADER, vtxShSrc);
-	var fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShSrc);
-
-	return createProgram(gl, vtxShader, fragShader);
+function loadTextures() {
+    if (loadTexs == texSrc.length) {
+        initGL();
+        configScene();
+        draw();
+    }
 }
 
-/**
- * Inicializa área de desenho: viewport e cor de limpeza; limpa a tela
- * @param {WebGLRenderingContext} gl 
- */
-function initScreen(gl) {
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-	gl.clearColor(0, 0, 0, 1);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.enable(gl.BLEND);
-	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+function initGL() {
+
+    var canvas = document.getElementById("glcanvas1");
+
+    gl = getGL(canvas);
+    if (gl) {
+        //Inicializa shaders
+        var vtxShSrc = document.getElementById("vertex-shader").text;
+        var fragShSrc = document.getElementById("frag-shader").text;
+
+        var vtxShader = createShader(gl, gl.VERTEX_SHADER, vtxShSrc);
+        var fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShSrc);
+        prog = createProgram(gl, vtxShader, fragShader);
+
+        gl.useProgram(prog);
+
+        //Inicializa área de desenho: viewport e cor de limpeza; limpa a tela
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.clearColor(0, 0, 0, 1);
+        gl.enable(gl.BLEND);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    }
 }
 
-function triangles() {
-	return new Float32Array([
-		-0.5, -0.5, 1.0, 0.0, 0.0, 1.0,
-		0.5, -0.5, 0.0, 1.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
-		-1.0, 0.0, 1.0, 1.0, 0.0, 0.7,
-		1.0, 0.0, 1.0, 0.0, 1.0, 0.7,
-		0.0, 0.5, 0.0, 1.0, 1.0, 0.7
-	]);
+function submitTexture(idx, img) {
+    var tex0 = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0 + idx);
+    gl.bindTexture(gl.TEXTURE_2D, tex0);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
 }
 
-function square() {
-	return new Float32Array([
-		-0.5, 0.5, 1.0, 1.0, 1.0, 1.0,
-		-0.5, -0.5, 1.0, 1.0, 1.0, 1.0,
-		0.5, -0.5, 1.0, 1.0, 1.0, 1.0,
-		// 0.5, -0.5, 1.0, 1.0, 1.0, 1.0, --> Repetindo o drawArrays do webgl não precisa dessa redundância
-		0.5, 0.5, 1.0, 1.0, 1.0, 1.0,
-		-0.5, 0.5, 1.0, 1.0, 1.0, 1.0
-	]);
-}
-
-/** Quadrado com textura */
-function square_with_tex() {
-	return new Float32Array([
-		-0.5, 0.5, 0.0, 0.0,
-		-0.5, -0.5, 0.0, 1.0,
-		0.5, -0.5, 1.0, 1.0,
-		0.5, 0.5, 1.0, 0.0,
-		-0.5, 0.5, 1.0, 1.0,
-	]);
-}
-
-/**
- * Desenha dois triângulos
- * @param {WebGLRenderingContext} gl 
- */
-function draw_triangles(gl) {
-	gl.drawArrays(gl.TRIANGLES, 0, 6); // a partir do vértice 0, desenha 2 triângulos (6 vértices)
-}
-
-/**
- * Desenha um quadrado
- * @param {WebGLRenderingContext} gl 
- */
-function draw_square(gl) {
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
-	gl.drawArrays(gl.TRIANGLES, 2, 6); // vai repetir o vértice 3 (para não precisar ser redundante no quadrado)
-}
 
 /**
  * 
- * @param {WebGLRenderingContext} gl 
- * @param {WebGLProgram} prog
  * @param {string} attributeName 
  * @param {number} dataQtde 
  * @param {number} blockSize 
  * @param {number} initialJump 
  */
-function setAttributePointer(gl, prog, attributeName, dataQtde, blockSize, initialJump) {
-	//Pega ponteiro para o atributo do vertex shader
-	var pointer = gl.getAttribLocation(prog, attributeName);
+function setAttributePointer(attributeName, dataQtde, blockSize, initialJump) {
+    //Pega ponteiro para o atributo do vertex shader
+    var pointer = gl.getAttribLocation(prog, attributeName);
 
-	gl.enableVertexAttribArray(pointer);
-	//Especifica a cópia dos valores do buffer para o atributo
-	gl.vertexAttribPointer(pointer,
-		dataQtde,        //quantidade de dados em cada processamento
-		gl.FLOAT, //tipo de cada dado (tamanho)
-		false,    //não normalizar
-		blockSize * 4,      //tamanho do bloco de dados a processar em cada passo
-		//0 indica que o tamanho do bloco é igual a tamanho
-		//lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
-		initialJump * 4         //salto inicial (em bytes)
-	);
+    gl.enableVertexAttribArray(pointer);
+    //Especifica a cópia dos valores do buffer para o atributo
+    gl.vertexAttribPointer(pointer,
+        dataQtde,        //quantidade de dados em cada processamento
+        gl.FLOAT, //tipo de cada dado (tamanho)
+        false,    //não normalizar
+        blockSize * 4,      //tamanho do bloco de dados a processar em cada passo
+        //0 indica que o tamanho do bloco é igual a tamanho
+        //lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
+        initialJump * 4         //salto inicial (em bytes)
+    );
 }
 
-/**
- * Cria buffer na GPU e copia coordenadas para ele
- * @param {WebGLRenderingContext} gl 
- * @param {Float32Array} coord 
- */
-function addCoordinatesToBuffer(gl, coord) {
-	var bufPtr = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, bufPtr);
-	gl.bufferData(gl.ARRAY_BUFFER, coord, gl.STATIC_DRAW);
+function configScene() {
+    //Define coordenadas dos triângulos
+    var coordTriangles = new Float32Array([
+        -0.5, 0.5, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 1.0,
+        0.5, -0.5, 1.0, 1.0,
+        0.5, 0.5, 1.0, 0.0,
+        -0.5, 0.5, 0.0, 0.0
+    ]);
+
+    //Cria buffer na GPU e copia coordenadas para ele
+    var bufPtr = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufPtr);
+    gl.bufferData(gl.ARRAY_BUFFER, coordTriangles, gl.STATIC_DRAW);
+
+    setAttributePointer("position", 2, 4, 0)
+    setAttributePointer("texCoord", 2, 4, 2)
+
+    //submeter textura para gpu
+    submitTexture(0, teximg[0])
+    submitTexture(1, teximg[1])
 }
 
-/**
- * 
- * @param {WebGLRenderingContext} gl 
- * @param {WebGLProgram} prog
- */
-function drawNoTex(gl, prog) {
-	//Define coordenadas dos triângulos e adiciona ao buffer
-	var coordTriangles = square()
-	addCoordinatesToBuffer(gl, coordTriangles)
+function draw() {
+    var matrot = [Math.cos(angle * Math.PI / 180.0), -Math.sin(angle * Math.PI / 180.0), 0.0, 0.0,
+    Math.sin(angle * Math.PI / 180.0), Math.cos(angle * Math.PI / 180.0), 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0];
 
-	setAttributePointer(gl, prog, "position", 2, 6, 0)
-	setAttributePointer(gl, prog, "color", 4, 6, 2)
+    transfPtr = gl.getUniformLocation(prog, "transf");
+    gl.uniformMatrix4fv(transfPtr, false, matrot);
 
-	initScreen(gl)
-	draw_square(gl)
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //desenha triângulos - executa shaders
+    var texPtr = gl.getUniformLocation(prog, "tex");
+    gl.uniform1i(texPtr, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.uniform1i(texPtr, 1);
+    gl.drawArrays(gl.TRIANGLES, 2, 3);
+
+    angle++;
+
+    requestAnimationFrame(draw);
 }
 
-/**
- * 
- * @param {WebGLRenderingContext} gl 
- * @param {HTMLImageElement} image 
- */
-function submitTexture(gl, image) {
-	var tex = gl.createTexture();
-
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, tex);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-}
-
-/**
- * 
- * @param {WebGLRenderingContext} gl 
- * @param {WebGLProgram} prog
- */
-function drawWithTex(gl, prog) {
-	var texImg = new Image()
-	texImg.src = "img/stick.png"
-
-	texImg.onload = () => {
-		//Define coordenadas dos triângulos e adiciona ao buffer
-		var coordTriangles = square_with_tex()
-		addCoordinatesToBuffer(gl, coordTriangles)
-
-		setAttributePointer(gl, prog, "position", 2, 6, 0)
-		setAttributePointer(gl, prog, "texCoord", 2, 4, 2)
-
-		submitTexture(gl, texImg)
-
-		// gl.activeTexture(gl.TEXTURE1);
-		// submitTexture(gl, texImg)
-
-		initScreen(gl)
-		draw_square(gl)
-	}
-
-
-}
 
 function init() {
-	/** @type {HTMLCanvasElement} */
-	var canvas = document.getElementById("glcanvas1");
-
-	var gl = getGL(canvas);
-	var prog = getProgram(gl)
-
-	gl.useProgram(prog);
-
-	drawWithTex(gl, prog)
+    for (i = 0; i < texSrc.length; i++) {
+        teximg[i] = new Image();
+        teximg[i].src = texSrc[i];
+        teximg[i].onload = function () {
+            loadTexs++;
+            loadTextures();
+        }
+    }
 }
