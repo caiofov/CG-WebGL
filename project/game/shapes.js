6cf1e98b8dad8636b5e46db435551c3bb8be97b7
caiofov <cfoviana@gmail.com>
2023-07-07 23:21:22 -0300

    vertices de um paralelepípedo

--- project/game/shapes.js
+++ project/game/shapes.js
@@ -1,30 +1,3 @@
-function box(init, size) {
-
-    return new Float32Array([
-        //Quad 1
-        //x         y       z       tx   ty
-        -init[0], init[1], init[2], 0.0, 0.0,
-        -init[0], -init[1], init[2], 0.0, 1.0,
-        init[0], -init[1], init[2], 1.0, 1.0,
-        init[0], init[1], init[2], 1.0, 0.0,
-        -init[0], init[1], init[2], 0.0, 0.0,
-
-        //Quad 2
-        -init[0], -init[1], init[2], 1.0, 1.0,
-        -init[0], init[1], init[2], 1.0, 0.0,
-        -init[0], init[1], init[2] + size, 0.0, 0.0,
-        -init[0], -init[1], init[2] + size, 0.0, 1.0,
-        -init[0], -init[1], init[2], 1.0, 1.0,
-
-        //Quad 3
-        init[0], -init[1], init[2] + size, 1.0, 1.0,
-        init[0], -init[1], init[2], 1.0, 0.0,
-        -init[0], -init[1], init[2], 0.0, 0.0,
-        -init[0], -init[1], init[2] + size, 0.0, 1.0,
-        init[0], -init[1], init[2] + size, 1.0, 1.0
-    ]);
-}
-
 /**
  * Desenha um quadrado (dois triângulos)
  * @param {number} startIdx índice do primeiro vértice do quadrado no array de vértices
@@ -32,4 +5,59 @@ function box(init, size) {
 function drawSquare(startIdx) {
     gl.drawArrays(gl.TRIANGLES, startIdx, 3);
     gl.drawArrays(gl.TRIANGLES, startIdx + 2, 3);
+}
+
+/**
+ * Cálcula os vértices de um paralelepípedo
+ * @param {number[]} pos Posição inicial do paralelepípedo
+ * @param {number} h altura
+ * @param {number} w largura
+ * @param {number} d profundidade
+ */
+function parallelepiped(pos, h, w, d) {
+    const [x, y, z] = pos
+    return new Float32Array([
+        //Trás
+        //x    y  z  tx   ty
+        x - w, y, z, 0.0, 0.0,
+        x - w, y - h, z, 0.0, 1.0,
+        x, y - h, z, 1.0, 1.0,
+        x, y, z, 1.0, 0.0,
+        x - w, y, z, 0.0, 0.0,
+
+        //Frente
+        x - w, y, z + d, 0.0, 0.0,
+        x - w, y - h, z + d, 0.0, 1.0,
+        x, y - h, z + d, 1.0, 1.0,
+        x, y, z + d, 1.0, 0.0,
+        x - w, y, z + d, 0.0, 0.0,
+
+        //Esquerda
+        x - w, y - h, z, 1.0, 1.0,
+        x - w, y, z, 1.0, 0.0,
+        x - w, y, z + d, 0.0, 0.0,
+        x - w, y - h, z + d, 0.0, 1.0,
+        x - w, y - h, z, 1.0, 1.0,
+
+        //Direita
+        x, y - h, z, 1.0, 1.0,
+        x, y, z, 1.0, 0.0,
+        x, y, z + d, 0.0, 0.0,
+        x, y - h, z + d, 0.0, 1.0,
+        x, y - h, z, 1.0, 1.0,
+
+        //Baixo
+        x, y - h, z + d, 1.0, 1.0,
+        x, y - h, z, 1.0, 0.0,
+        x - w, y - h, z, 0.0, 0.0,
+        x - w, y - h, z + d, 0.0, 1.0,
+        x, y - h, z + d, 1.0, 1.0,
+
+        //Cima
+        x, y, z + d, 1.0, 1.0,
+        x, y, z, 1.0, 0.0,
+        x - w, y, z, 0.0, 0.0,
+        x - w, y, z + d, 0.0, 1.0,
+        x, y, z + d, 1.0, 1.0
+    ]);
 }
