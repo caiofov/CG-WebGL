88f651f6b124936f00943bea0962c5564e1b5eae
caiofov <cfoviana@gmail.com>
2023-07-08 19:08:53 -0300

    refatorar read obj file

--- project/game/readObj.js
+++ project/game/readObj.js
@@ -8,33 +8,23 @@ function readObjFile(filePath) {
         fetch(filePath)
             .then((r) => r.text())
             .then((content) => {
-                const lines = content.split('\n');
                 const vertices = [];
-                const faces = [];
 
-                for (let i = 0; i < lines.length; i++) {
-                    const line = lines[i].trim().split(' ');
-                    const type = line.shift();
+                for (const lines of content.split('\n')) {
+                    for (let i = 0; i < lines.length; i++) {
+                        const line = lines[i].trim().split(' ');
+                        const type = line.shift();
 
-                    switch (type) {
-                        case "v":
-                            vertices.push(parseFloat(line[0])) //x
-                            vertices.push(parseFloat(line[1])) //y
-                            vertices.push(parseFloat(line[2])) //z
-                            vertices.push(parseFloat(line[3])) //tx
-                            vertices.push(parseFloat(line[4])) //ty
-                            break;
-                        case "f":
-                            const face = line.map((vertice) => {
-                                const [index, , normal] = vertice.split('/');
-                                return { index: parseInt(index) - 1, normal: parseInt(normal) - 1 };
-                            });
-                            faces.push(face);
-                            break;
-                        default:
-                            break;
+                        switch (type) {
+                            case "v":
+                                for (let i = 0; i < 5; i++) {
+                                    vertices.push(parseFloat(line[i]))
+                                }
+                                break;
+                            default:
+                                break;
+                        }
                     }
-
                 }
 
                 resolve(vertices);
