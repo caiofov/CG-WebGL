aa9321a82c09edbd8cbbb27133a741d541e27a2d
caiofov <cfoviana@gmail.com>
2023-07-15 00:44:59 -0300

    ler faces no obj

--- project/game/drawing/readObj.js
+++ project/game/drawing/readObj.js
@@ -12,21 +12,32 @@ function readObjFile(filePath) {
                 const faces = []
                 const lines = content.split('\n') //separa o conteúdo em linhas
                 for (const line of lines) {
+                    if (line.startsWith("#")) continue
                     const vs = line.trim().split(' ');
+                    const type = vs.shift()
 
-                    switch (vs.shift()) { // vê o tipo do primeiro elemento da linha
+                    switch (type) { // vê o tipo do primeiro elemento da linha
                         case "v": //vértices
-                            for (let i = 0; i < 5; i++) vertices.push(parseFloat(vs[i]))
+                            console.log(vs)
+                            for (const v of vs) vertices.push(parseFloat(v))
+
+                            vertices.push(0.0)
+                            vertices.push(1.0)
+                            console.log(vertices)
                             break;
+
                         case "f": //faces
+                            for (const l of vs) {
+                                let li = parseInt(l) - 1
+                                for (let i = 0; i < 5; i++) faces.push(vertices[li * 5 + i])
+                            }
                             break
                         default:
                             break;
                     }
                 }
-                resolve(vertices);
-
-                return vertices
+                resolve(faces);
+                return faces
             })
 
 
