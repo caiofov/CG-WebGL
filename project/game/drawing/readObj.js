0f5d34254c35c055f084b55d953baf180096ae6f
caiofov <cfoviana@gmail.com>
2023-07-15 01:52:15 -0300

    lidar com textura em arquivo obj

--- project/game/drawing/readObj.js
+++ project/game/drawing/readObj.js
@@ -10,6 +10,7 @@ function readObjFile(filePath) {
             .then((content) => {
                 const vertices = [];
                 const faces = []
+                const textures = []
                 const lines = content.split('\n') //separa o conteúdo em linhas
                 for (const line of lines) {
                     if (line.startsWith("#")) continue
@@ -18,18 +19,20 @@ function readObjFile(filePath) {
 
                     switch (type) { // vê o tipo do primeiro elemento da linha
                         case "v": //vértices
-                            console.log(vs)
-                            for (const v of vs) vertices.push(parseFloat(v))
-
-                            vertices.push(0.0)
-                            vertices.push(1.0)
-                            console.log(vertices)
+                            const vertx = []
+                            for (const v of vs) vertx.push(parseFloat(v))
+                            vertices.push(vertx)
                             break;
+
+                        case "vt": //textura
+                            textures.push([parseFloat(vs[0]), parseFloat(vs[1])])
+                            break
 
                         case "f": //faces
                             for (const l of vs) {
                                 let li = parseInt(l) - 1
-                                for (let i = 0; i < 5; i++) faces.push(vertices[li * 5 + i])
+                                for (const v of vertices[li]) faces.push(v)
+                                for (const t of textures[li]) faces.push(t)
                             }
                             break
                         default:
