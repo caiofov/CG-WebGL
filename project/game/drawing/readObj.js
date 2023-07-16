544f80c381502f57838485d92f007aecb239c112
caiofov <cfoviana@gmail.com>
2023-07-15 22:04:04 -0300

    leitor de obj adaptador para normais

--- project/game/drawing/readObj.js
+++ project/game/drawing/readObj.js
@@ -1,48 +1,56 @@
 /**
  * Ler um arquivo `.obj`
  * @param {string} filePath Caminho ate o arquivo
- * @returns 
+ * @returns {Promise<object>}
  */
 function readObjFile(filePath) {
-    return new Promise((resolve) => {
-        fetch(filePath)
-            .then((r) => r.text())
-            .then((content) => {
-                const vertices = [];
-                const faces = []
-                const textures = []
-                const lines = content.split('\n') //separa o conteúdo em linhas
-                for (const line of lines) {
-                    if (line.startsWith("#")) continue
-                    const vs = line.trim().split(' ');
-                    const type = vs.shift()
+    return fetch(filePath)
+        .then((r) => r.text())
+        .then((content) => {
+            const vertices = [];
+            const faces = []
+            const textures = []
+            const normals = []
+            const orderedNormals = []
+            content = content.replace('//', ' ')
+            for (const line of content.split('\n')) { //separa o conteúdo em linhas
+                if (line.startsWith("#")) continue //comentários
+                const vs = line.trim().split(' ');
+                const type = vs.shift()
 
-                    switch (type) { // vê o tipo do primeiro elemento da linha
-                        case "v": //vértices
-                            const vertx = []
-                            for (const v of vs) vertx.push(parseFloat(v))
-                            vertices.push(vertx)
-                            break;
+                switch (type) { // vê o tipo do primeiro elemento da linha
+                    case "v": //vértices
+                        const vertx = []
+                        for (const v of vs) vertx.push(parseFloat(v))
+                        vertices.push(vertx)
+                        break;
 
-                        case "vt": //textura
-                            textures.push([parseFloat(vs[0]), parseFloat(vs[1])])
-                            break
+                    case "vn": //normal:
+                        const nrms = []
+                        for (const n of vs) nrms.push(parseFloat(n))
+                        normals.push(nrms)
+                        break
 
-                        case "f": //faces
-                            for (const l of vs) {
-                                let li = parseInt(l) - 1
-                                for (const v of vertices[li]) faces.push(v)
-                                for (const t of textures[li]) faces.push(t)
-                            }
-                            break
-                        default:
-                            break;
-                    }
+                    case "vt": //textura
+                        textures.push([parseFloat(vs[0]), parseFloat(vs[1])])
+                        break
+
+                    case "f": //faces
+                        for (const l of vs) {
+                            let li = parseInt(l) - 1
+                            if (vertices[li]) for (const v of vertices[li]) faces.push(v)
+                            if (textures[li]) for (const t of textures[li]) faces.push(t)
+                            if (normals[li]) for (const n of normals[li]) orderedNormals.push(n)
+                        }
+                        break
+                    default:
+                        break;
                 }
-                resolve(faces);
-                return faces
-            })
+            }
+
+            return { faces: faces, normals: orderedNormals }
+        })
 
 
-    })
+
 }
