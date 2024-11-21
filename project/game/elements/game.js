13fa5489766cf721fdfb80460b2be80e7b5fa1aa
Caio Oliveira <cfoviana@alu.ufc.br>
2024-11-21 17:01:00 -0300

    armazenar melhor pontuação

--- project/game/elements/game.js
+++ project/game/elements/game.js
@@ -4,21 +4,35 @@ const GAME = {
     record: false
 }
 
+const RECORD = Number(localStorage.getItem('record')) ?? 0
 
 /**@type {HTMLParagraphElement[]} */
-const SCOREBOARD = document.querySelectorAll(".scoreboard")
+const SCOREBOARD = document.querySelector("#live-scoreboard")
 /**@type {HTMLDivElement} */
 const GAME_OVER_MESSAGE = document.querySelector("#game-over-overlay")
 
 /**Atualiza a pontuação */
 function score() {
     GAME.scores += 1
-    SCOREBOARD.map(p => p.innerHTML = GAME.scores)
+    if (!GAME.record && GAME.scores > RECORD) {
+        GAME.record = true
+        document.querySelectorAll(".new-record").forEach(r => r.className = r.className.replace('d-none', 'd-flex'))
+    } else if (GAME.record) {
+        document.querySelector("#record").innerHTML = GAME.scores
+    }
+    SCOREBOARD.innerHTML = GAME.scores
 }
 
 /** Exibe mensagem de game over e para de rodar a animação */
 function gameOver() {
     GAME.isRunning = false
+    if (GAME.record) {
+        localStorage.setItem('record', GAME.scores)
+        document.querySelector("#record").innerHTML = GAME.scores
+    }
+
+    document.querySelector("#end-scoreboard").innerHTML = GAME.scores
+
     document.querySelectorAll(".show-end").forEach(e => e.className = e.className.replace("d-none", "d-flex"))
 
     // var loseContextExtension = gl.getExtension('WEBGL_lose_context');
