174584e7383c425a47a6cbc30ebefd57760f5dd8
Caio Oliveira <cfoviana@alu.ufc.br>
2024-11-21 15:18:29 -0300

    organizar mensagens no canvas

--- project/game/elements/game.js
+++ project/game/elements/game.js
@@ -1,28 +1,25 @@
 const GAME = {
     scores: 0,
-    isRunning: false
+    isRunning: false,
+    record: false
 }
 
 
-/**@type {HTMLParagraphElement} */
-const scoreboard = document.querySelector("#scoreboard")
+/**@type {HTMLParagraphElement[]} */
+const SCOREBOARD = document.querySelectorAll(".scoreboard")
 /**@type {HTMLDivElement} */
 const GAME_OVER_MESSAGE = document.querySelector("#game-over-overlay")
 
 /**Atualiza a pontuação */
 function score() {
     GAME.scores += 1
-    scoreboard.innerHTML = GAME.scores
+    SCOREBOARD.map(p => p.innerHTML = GAME.scores)
 }
 
 /** Exibe mensagem de game over e para de rodar a animação */
 function gameOver() {
-    GAME_OVER_MESSAGE.className = GAME_OVER_MESSAGE.className.replace("d-none", "d-flex")
     GAME.isRunning = false
-
-    /**@type {HTMLDivElement} */
-    const overlay = document.querySelector("#overlay")
-    overlay.className = overlay.className.replace("d-none", "d-flex")
+    document.querySelectorAll(".show-end").forEach(e => e.className = e.className.replace("d-none", "d-flex"))
 
     // var loseContextExtension = gl.getExtension('WEBGL_lose_context');
     // if (loseContextExtension) {
